15eb8f222aa063523b976cd9ffef972d089a5155
Ivan Malets <operbober@mail.ru>
2017-05-06 15:11:11 +0300

    clean up project

--- src/main/java/by/tasktracker/service/ProjectTagService.java
+++
@@ -1,6 +0,0 @@
-package by.tasktracker.service;
-
-import by.tasktracker.entity.ProjectTag;
-import by.tasktracker.service.supeclass.NamedService;
-
-public interface ProjectTagService extends NamedService<ProjectTag> {}
